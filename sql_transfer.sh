1e8d2f2c0ccd9d2201ae24deddd0832ed1be32bc
nategd <nategd@umich.edu>
2016-06-13 17:38:03 -0400

    final

---
+++ sql_transfer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#This file will transfer the LIVE database to the MAMP local host
+# Created by Nathan G. Donaldson
+
+
+DB=#EDIT
+HOST=#EDIT
+PATH=~/  #EDIT OPTIONAL
+cd $PATH
+
+ssh -t HOST "mysqldump $DB" > $DB.sql
+echo "$DB successfully downloaded."
+echo "Sourcing $DB to MAMP localhost. This will take a few minutes."
+mysql --host=localhost -uroot -proot <<QUERY_INPUT
+SET GLOBAL max_allowed_packet=18446744073709551615;
+DROP DATABASE IF EXISTS $DB;
+CREATE DATABASE $DB;
+connect $DB
+source $DB.sql
+QUERY_INPUT
+echo "Deleting the temporary $DB file"
+rm $DB.sql
