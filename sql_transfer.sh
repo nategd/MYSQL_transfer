ee1a22041f7ce8dacf86aafe60b022ae6abc04c0
Nathan Donaldson <nategd@umich.edu>
2016-06-16 16:08:24 -0400

    Update sql_transfer.sh
    Fixed a display error

--- sql_transfer.sh
+++ sql_transfer.sh
@@ -10,7 +10,7 @@ cd $PATH
 
 ssh -t HOST "mysqldump $DB" > $DB.sql
 echo "$DB successfully downloaded."
-echo "Sourcing $DB to MAMP localhost. This will take a few minutes."
+echo "Sourcing $DB to mysql at path $PATH. This will take a few minutes."
 mysql --host=localhost -uroot -proot <<QUERY_INPUT
 SET GLOBAL max_allowed_packet=18446744073709551615;
 DROP DATABASE IF EXISTS $DB;
